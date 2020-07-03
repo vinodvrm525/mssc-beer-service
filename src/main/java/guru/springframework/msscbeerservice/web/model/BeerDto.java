15b0192a35c58b14fdc3dea954863d47feefd009
vinodvrm525 <vinodverma525@gmail.com>
2020-07-04 00:06:10 +0530

    adding web data model. close #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,31 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID uuid;
+    private Integer version;
+
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+    private String BeerName;
+
+    private BeerStyle beerStyle;
+    private Long upc;
+
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER, PISLSNER, ALE, STOUT, GOSE, IPA
+}
